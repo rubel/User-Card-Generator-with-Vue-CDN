0d0c82972cf70b026217034a69208712d13d8d98
rubel <rubel_xpress@yahoo.com>
2022-11-06 07:55:53 +0600

    random user card generator with vue cdn

---
+++ app.js
@@ -0,0 +1,33 @@
+var app = Vue.createApp({
+    data(){
+        return{
+            firstName: "Rubel",
+            lastName: "Hamza",
+            email: "rubel_xpress@yahoo.com",
+            gender:"male",
+            cell:"01912429159",
+            dob: "1987-03-27",
+            location: "Dhaka, Bangladesh",
+            image:"https://randomuser.me/api/portraits/men/10.jpg"
+        }
+    },
+    methods:{
+        async getUser(){
+            let res = await fetch("https://randomuser.me/api/");
+            let {results} = await res.json();
+            let user = results[0];
+            console.log(user);
+
+            this.firstName = user.name.first;
+            this.lastName = user.name.last;
+            this.email = user.email;
+            this.gender = user.gender;
+            this.cell = user.cell;
+            this.dob =  user.dob.date;
+            this.location = `${user.location.city}, ${user.location.country}`;
+            this.image = user.picture.large;
+        }
+    }
+});
+
+app.mount("#app");
